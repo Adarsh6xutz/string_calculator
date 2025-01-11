496734cf77bc98051906581c5717b6d34d4cd241
AShrivastav <adarshsrivastav899@gmail.com>
2025-01-11 16:39:53 +0530

    Add test for multiple numbers with comma delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,15 @@ RSpec.describe StringCalculator do
       expect(calculator.add("1")).to eq(1)
       expect(calculator.add("5")).to eq(5)
     end
+
+    it 'returns sum for two numbers' do
+      expect(calculator.add("1,2")).to eq(3)
+      expect(calculator.add("5,7")).to eq(12)
+    end
+
+    it 'returns sum for multiple numbers' do
+      expect(calculator.add("1,2,3")).to eq(6)
+      expect(calculator.add("5,7,8,10")).to eq(30)
+    end
   end
 end
