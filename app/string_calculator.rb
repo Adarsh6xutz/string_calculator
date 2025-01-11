74cc3fe0947df10cd8ca2e3c72ec73b0a2845dcf
AShrivastav <adarshsrivastav899@gmail.com>
2025-01-11 18:12:12 +0530

    Add support for negative numbers

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -4,12 +4,25 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
+    numbers_array = sanitized_numbers(numbers)
+    check_for_negatives(numbers_array)
+    numbers_array.sum
+  end
+
+  private
+
+  def sanitized_numbers(numbers)
     if numbers.start_with?("//")
       delimiter = numbers[2]
       numbers_part = numbers.split("\n", 2)[1]
-      numbers_part.gsub(delimiter, ",").split(',').map(&:to_i).sum
+      numbers_part.gsub(delimiter, ",").split(',').map(&:to_i)
     else
-      numbers.gsub("\n", ",").split(',').map(&:to_i).sum
+      numbers.gsub("\n", ",").split(',').map(&:to_i)
     end
+  end
+
+  def check_for_negatives(numbers)
+    negatives = numbers.select { |n| n < 0 }
+    raise NegativesNotAllowedException, "negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
   end
 end
