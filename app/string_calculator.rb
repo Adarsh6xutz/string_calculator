7915756dc6023aa736e31af544f9574de40a4606
AShrivastav <adarshsrivastav899@gmail.com>
2025-01-11 16:35:35 +0530

    Add support for single number input

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    0
+
+    numbers.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,8 +5,13 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
   
   describe '#add' do
-    it 'returns 0 for empty string' do
+    it 'Should return 0 for empty string' do
       expect(calculator.add("")).to eq(0)
+    end
+
+    it 'Should return the number for single number input' do
+      expect(calculator.add("1")).to eq(1)
+      expect(calculator.add("5")).to eq(5)
     end
   end
 end
