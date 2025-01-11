ff97bec5c80b029476d16bf1deee187c4bad8688
AShrivastav <adarshsrivastav899@gmail.com>
2025-01-11 17:11:39 +0530

    Add test for custom delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -29,5 +29,12 @@ RSpec.describe StringCalculator do
       expect(calculator.add("1,2\n3")).to eq(6)
       expect(calculator.add("1\n2\n3")).to eq(6)
     end
+
+    it 'should return sum for numbers ignoring any custom delimiters' do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+      expect(calculator.add("//*\n1*2*3")).to eq(6)
+      expect(calculator.add("//|\n1|2|3")).to eq(6)
+      expect(calculator.add("//&\n1&2&3")).to eq(6)
+    end
   end
 end
